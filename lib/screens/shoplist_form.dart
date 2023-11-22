2476fa38d246805241317284e46d269d4a3e7019
michelleangelicas <michelleangelica1907@gmail.com>
2023-11-22 13:25:05 +0700

    tutorial 9

--- lib/screens/shoplist_form.dart
+++ lib/screens/shoplist_form.dart
@@ -125,7 +125,7 @@ class _ShopFormPageState extends State<ShopFormPage> {
                               // Kirim ke Django dan tunggu respons
                               // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
                               final response = await request.postJson(
-                              "http://darryl-nawawi-tutorial.pbp.cs.ui.ac.id/create-flutter/",
+                              "http://michelle-angelica21-tutorial.pbp.cs.ui.ac.id/create-flutter/",
                               jsonEncode(<String, String>{
                                   'name': _name,
                                   'price': _price.toString(),
